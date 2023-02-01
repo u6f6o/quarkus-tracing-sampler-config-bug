8dca2344fcac4e659d10c52fdf9fb96fbe6f5412
Ulf Gitschthaler <ulf.gitschthaler@exaring.de>
2023-02-01 13:10:29 +0100

    Initial commits

---
+++ src/native-test/java/com/u6f6o/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.u6f6o;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
